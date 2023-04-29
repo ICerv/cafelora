fa2d4b6699e03bf5bf132be8239a89710f8af11a
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-29 12:11:10 +0200

    Activate navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import './Header.css';
 export const Header = () => {
 
-  const element = document.createElement('header')
+  const element = document.createElement('header');
 
   element.innerHTML = `
       <div class="header__content container" id="home">
@@ -16,15 +16,14 @@ export const Header = () => {
           </nav>
           </div>
       </div>
-  `
-
+  `;
   element.querySelector('.nav-btn').addEventListener('click', () => {
     element.querySelector('.rollout-nav').classList.toggle('nav-closed')
-  })
+  });
 
   element.querySelector('.rollout-nav').addEventListener('click', () => {
     element.querySelector('nav').classList.add('nav-closed')
-  })
+  });
 
-  return element
+  return element;
 }
